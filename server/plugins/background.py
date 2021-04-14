a903789303e5826c2f65159738fc835d44d2e912
Daniel Gruno <humbedooh@apache.org>
2021-04-14 19:08:17 +0200

    PEP8 linting

--- server/plugins/background.py
+++ server/plugins/background.py
@@ -13,6 +13,7 @@ import plugins.github
 
 class ProgTimer:
     """A simple task timer that displays when a sub-task is begun, ends, and the time taken."""
+
     start: float
     title: str
 
@@ -20,17 +21,12 @@ class ProgTimer:
         self.title = title
 
     async def __aenter__(self):
-        sys.stdout.write(
-            "[%s] %s...\n" % (datetime.datetime.now().strftime("%H:%M:%S"), self.title)
-        )
+        sys.stdout.write("[%s] %s...\n" % (datetime.datetime.now().strftime("%H:%M:%S"), self.title))
         sys.stdout.flush()
         self.start = time.time()
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        print(
-            "[%s] Done in %.2f seconds"
-            % (datetime.datetime.now().strftime("%H:%M:%S"), time.time() - self.start)
-        )
+        print("[%s] Done in %.2f seconds" % (datetime.datetime.now().strftime("%H:%M:%S"), time.time() - self.start))
 
 
 async def adjust_teams(server: plugins.basetypes.Server):
@@ -40,7 +36,7 @@ async def adjust_teams(server: plugins.basetypes.Server):
     """
     async with ProgTimer("Adjusting GitHub teams according to LDAP/MFA"):
         for team in server.data.teams:
-            if team.type == 'committers':  # All we care about right now is the commit groups
+            if team.type == "committers":  # All we care about right now is the commit groups
                 asf_project = server.data.projects.get(team.project)
                 if asf_project:
                     if asf_project.public_repos:
@@ -58,7 +54,7 @@ async def adjust_teams(server: plugins.basetypes.Server):
                     print(f"Could not find an ASF project for team {team.slug}!!")
 
             # PMC Groups
-            elif team.type == 'pmc':  # All we care about right now is the commit groups
+            elif team.type == "pmc":  # All we care about right now is the commit groups
                 asf_project = server.data.projects.get(team.project)
                 if asf_project:
                     if asf_project.private_repos:
@@ -83,7 +79,9 @@ async def adjust_repositories(server: plugins.basetypes.Server):
             if team.type == "committers":
                 asf_project = server.data.projects.get(team.project)
                 if asf_project:
-                    managed_repos = [x.filename for x in asf_project.public_repos if x.filename in server.data.github_repos]
+                    managed_repos = [
+                        x.filename for x in asf_project.public_repos if x.filename in server.data.github_repos
+                    ]
                     added, removed = await team.set_repositories(managed_repos)
                     for repo in added:
                         print(f"- Added {repo}.git to GitHub team {team.slug}")
@@ -92,7 +90,9 @@ async def adjust_repositories(server: plugins.basetypes.Server):
             elif team.type == "pmc":
                 asf_project = server.data.projects.get(team.project)
                 if asf_project:
-                    managed_repos = [x.filename for x in asf_project.private_repos if x.filename in server.data.github_repos]
+                    managed_repos = [
+                        x.filename for x in asf_project.private_repos if x.filename in server.data.github_repos
+                    ]
                     added, removed = await team.set_repositories(managed_repos)
                     for repo in added:
                         print(f"- Added {repo}.git to GitHub team {team.slug}")
@@ -110,7 +110,9 @@ async def run_tasks(server: plugins.basetypes.Server):
     while True:
         now = time.time()
         print(f"Processing GitHub organization '{server.config.github.org}'...")
-        asf_github_org = plugins.github.GitHubOrganisation(login=server.config.github.org, personal_access_token=server.config.github.token)
+        asf_github_org = plugins.github.GitHubOrganisation(
+            login=server.config.github.org, personal_access_token=server.config.github.token
+        )
         await asf_github_org.get_id()  # For security reasons, we must call this before we can add/remove members
         limit, used = await asf_github_org.rate_limit_rest()
         print("Used %u out of %u REST tokens this hour." % (used, limit))
@@ -121,10 +123,7 @@ async def run_tasks(server: plugins.basetypes.Server):
             try:
                 server.data.repositories = await plugins.repositories.list_all(server.config.repos)
             except Exception as e:
-                print(
-                    "Could not fetch repositories - source server down or not connected: %s"
-                    % e
-                )
+                print("Could not fetch repositories - source server down or not connected: %s" % e)
                 await asyncio.sleep(10)
                 continue
 
@@ -132,14 +131,13 @@ async def run_tasks(server: plugins.basetypes.Server):
             server.data.github_repos = await asf_github_org.load_repositories()
         async with ProgTimer("Compiling list of projects, repos and memberships"):
             try:
-                asf_org = await plugins.projects.compile_data(server.config.ldap, server.data.repositories, server.database.client)
+                asf_org = await plugins.projects.compile_data(
+                    server.config.ldap, server.data.repositories, server.database.client
+                )
                 server.data.projects = asf_org.projects
                 server.data.people = asf_org.committers
             except Exception as e:
-                print(
-                    "Could not fetch repositories - ldap source down or not connected: %s"
-                    % e
-                )
+                print("Could not fetch repositories - ldap source down or not connected: %s" % e)
 
         async with ProgTimer("Gathering MFA status of GitHub users"):
             server.data.mfa = await asf_github_org.get_mfa_status()
@@ -167,6 +165,7 @@ async def run_tasks(server: plugins.basetypes.Server):
         if used < aused:
             used_gql -= used
         time_taken = time.time() - now
-        print("Background task run finished after %u seconds. Used %u GraphQL tokens for this." % (time_taken, used_gql))
+        print(
+            "Background task run finished after %u seconds. Used %u GraphQL tokens for this." % (time_taken, used_gql)
+        )
         await asyncio.sleep(server.config.tasks.refresh_rate)
-
